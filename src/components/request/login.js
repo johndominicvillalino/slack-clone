f9edda1c110fe064b9434a8f56fa46a8176a7a20
John Villalino <johndominicvillalino@outlook.com>
2022-05-28 10:34:51 +0800

    add login request for neal

---
+++ src/components/request/login.js
@@ -0,0 +1,23 @@
+export const login = () => {
+
+    let data = {
+        email: "user333333332@example.com",
+        password: "12345678"
+    }
+
+    let requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        redirect: 'follow',
+        headers: {
+            'Content-Type': 'Application/json'
+        }
+
+      };
+      
+      fetch("http://206.189.91.54//api/v1/auth/sign_in", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+}
