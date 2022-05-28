20327044b64158fe0bff133b6c44b919f6a3ea79
BryBTG <brybtg@gmail.com>
2022-05-28 09:26:42 +0800

    Update App.js
    add router, routes waiting and components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
-import MessageDirect from './components/MessageDirect/MessageDirect';
+import "./App.css";
+import MessageDirect from "./components/MessageDirect/MessageDirect";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    < >
-    <MessageDirect></MessageDirect>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/message" component={MessageDirect} />
+          {/* <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} /> */}
+        </Switch>
+      </Router>
     </>
   );
 }
