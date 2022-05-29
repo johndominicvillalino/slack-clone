d89eabf6a0a5c7d13d0224b235cfc247e950cbff
JOhn V <johndominicvillalino@outlook.com>
2022-05-29 10:03:55 +0800

    create send message ui

--- src/components/MessageDirect/MessageDirect.js
+++ src/components/MessageDirect/MessageDirect.js
@@ -1,27 +1,84 @@
-import React, { useEffect } from 'react'
-import MessageCointaer from '../layouts/MessageContainer/MessageCointaer'
-import { sendMessage } from '../request/sendMessage'
+import React, { useEffect, useState, useRef } from "react";
+import {ReactComponent as SendLogo} from './send.svg'
+import './MessageDirect.css'
+
+
 
 const MessageDirect = () => {
+  const inputRef = useRef();
+
+  const [textScrollHeight, setTextScrollHeight] = useState("auto");
+  const [message,setMessage] = useState('')
+  const [sendClass, setSendClass] = useState('disable')
+  const [sendClassSpan, setSendClassSpan] = useState('sendMessageBtnDisable')
 
 
-  const handleSubmit = async e => {
-    
-    e.preventDefault()
-    const test = await sendMessage()
 
 
-  } 
+  useEffect(() => {
+    const currHeight = `${inputRef.current.scrollHeight}px`;
+    setTextScrollHeight(currHeight);
 
-  return ( 
+    return () => {};
+  }, [textScrollHeight]);
+
+
+  useEffect(() => {
+
+    if(message.length === 0) {
+      setSendClass('disable')
+      setSendClassSpan('sendMessageBtnDisable')
+    } else {
+      setSendClass('activate')
+      setSendClassSpan('sendMessageBtn')
+    }
+
+  },[message])
+
+ 
+
+  const onChangeHandler = (e) => {
+    const height = `${e.target.scrollHeight}px`;
+    const {value} = e.target
+    setMessage(value)
+    setTextScrollHeight(height);
+
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(message.length < 1) {
+      return
+    }
+    console.log(message)
+    // const test = await sendMessage();
+  };
+
+  return (
     <>
-    <MessageCointaer><button onClick={handleSubmit}>
-      submit
-      </button></MessageCointaer>
+      <div className="messageDirectContainer">
+        <div className="topOptions">
+        </div>
+        <form>
+         <textarea
+            className="messageText"
+            placeholder="Jot something down"
+            rows={1}
+            style={{
+              height: textScrollHeight,
+            }}
+            value={message}
+            onChange={onChangeHandler}
+            ref={inputRef}
+          ></textarea> 
+    
+          <span type='submit' onClick={handleSubmit} className={sendClassSpan}>
+             <SendLogo className={sendClass}></SendLogo>
+          </span>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-
-
-export default MessageDirect
+export default MessageDirect;
