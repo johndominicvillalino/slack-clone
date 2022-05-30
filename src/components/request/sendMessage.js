2b13354c1b7d0ced24210ec56d8d599f1205c15f
John Villalino <johndominicvillalino@outlook.com>
2022-05-30 19:29:51 +0800

    create send message function

--- src/components/request/sendMessage.js
+++ src/components/request/sendMessage.js
@@ -1,9 +1,53 @@
-import axios from "axios"
+
+// desc : send a direct message user 
+// Parameter : pass object data with properties - { accessToken, client, expiry, uid, message, receiver_id } 
+
+const sendMessage = async (userInfo) => {
+
+    try {
+
+        if (typeof userInfo !== "object") {
+            return 'Please pass an object!'
+        }
+
+        const { accessToken, client, expiry, uid, message, receiver_id } = userInfo;
 
 
-export const sendMesage = () => {
+        let data = {
+            receiver_id,
+            receiver_class: "User",
+            body: message
+        }
 
+        data = JSON.stringify(data)
 
+        let requestOptions = {
+            method: 'POST',
+            body: data,
+            redirect: 'follow',
+            headers: {
 
+                'access-token': accessToken,
+                'client': client,
+                'expiry': expiry,
+                'uid': uid,
+                'Content-Type': 'Application/json'
 
+            }
+        }
+
+        const res = await fetch("http://206.189.91.54//api/v1/messages", requestOptions)
+
+        if (res) {
+            console.log(await res.text())
+        }
+
+    } catch (err) {
+
+        console.error(err.message)
+
+    }
 }
+
+
+export default sendMessage
