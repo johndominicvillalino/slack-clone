58df6f46cc5c05023443980bb2b1ea85342479f7
Edcel Jake P. Aquino <aedceljake@gmail.com>
2022-05-30 21:40:10 +0800

    create request for login and register

--- src/components/request/register.js
+++ src/components/request/register.js
@@ -1,24 +1,24 @@
-const login = (email, password) => {
+const register = (email, password, password_confirmation) => {
 
     let data = {
         email: email,
         password: password,
+        password_confirmation: password_confirmation,
     };
 
-    let requestOptions = {
+    var requestOptions = {
         method: 'POST',
         body: JSON.stringify(data),
         redirect: 'follow',
         headers: {
             'Content-Type': 'Application/json'
         }
-
     };
 
-    return fetch("http://206.189.91.54//api/v1/auth/sign_in", requestOptions)
+    fetch("http://206.189.91.54//api/v1/auth/", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
 
-export default login;
+export default register;
